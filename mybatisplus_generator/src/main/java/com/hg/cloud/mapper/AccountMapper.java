a3f8d8128ae0ecd26bc173229fd46920c6f7360b
ygl <934752370@qq.com>
2024-03-12 19:30:19 +0800

    3.12

---
+++ mybatisplus_generator/src/main/java/com/hg/cloud/mapper/AccountMapper.java
@@ -0,0 +1,7 @@
+package com.hg.cloud.mapper;
+
+import com.hg.cloud.entities.Account;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface AccountMapper extends Mapper<Account> {
+}
---
+++ mybatisplus_generator/src/main/java/com/hg/cloud/mapper/StorageMapper.java
@@ -0,0 +1,7 @@
+package com.hg.cloud.mapper;
+
+import com.hg.cloud.entities.Storage;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface StorageMapper extends Mapper<Storage> {
+}
